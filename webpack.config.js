5fa748ce6d119979543efb346295ae0b6eb78a7a
Jayden Seric <me@jaydenseric.com>
2016-07-27 20:50:40 +1000

    Correct .js file extension to .jsx.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const reporter = require('postcss-reporter')
 
 const config = {
   entry: [
-    path.join(__dirname, 'src/client.js')
+    path.join(__dirname, 'src/client.jsx')
   ],
   output: {
     path: path.join(__dirname, 'public/bundle'),
