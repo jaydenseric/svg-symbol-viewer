afb9d291727bc0b22caa6f5589f7c13d0e3a6bf7
Jayden Seric <me@jaydenseric.com>
2016-09-01 01:27:27 +1000

    Minor tidy.

--- src/client.jsx
+++ src/client.jsx
@@ -1,7 +1,7 @@
 import 'babel-polyfill'
 import 'svgxuse'
 import React from 'react'
-import { render } from 'react-dom'
+import {render} from 'react-dom'
 import App from 'components/app'
 
 render(
