2ade28b24716bf848f564a64054b3ab01da3d8bb
Jayden Seric <me@jaydenseric.com>
2016-09-02 20:53:23 +1000

    Server now compresses requests.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const compression = require('compression')
 const path = require('path')
 
 const app = express()
+
+app.use(compression())
 
 app.use(express.static(path.resolve('./public')))
 
